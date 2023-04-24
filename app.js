c591bf394f7cb15298b579264e61ea8e88710e75
linhktran <tranlinh552003@gmail.com>
2023-04-24 22:21:52 +0300

    add files

---
+++ app.js
@@ -0,0 +1,33 @@
+import { serve } from "./deps.js";
+import { configure } from "./deps.js";
+import * as listController from "./controllers/listController.js";
+import * as itemController from "./controllers/itemController.js";
+import * as mainController from "./controllers/mainController.js";
+
+configure({
+  views: `${Deno.cwd()}/views/`,
+});
+
+const handleRequest = async (request) => {
+  const url = new URL(request.url);
+
+  if (url.pathname === "/" && request.method === "GET") {
+    return mainController.viewStats(request);
+  } else if (url.pathname === "/lists" && request.method === "POST") {
+    return await listController.addList(request);
+  } else if (url.pathname === "/lists" && request.method === "GET") {
+    return await listController.viewLists(request);
+  } else if (url.pathname.match("lists/[0-9]+") && request.method === "GET") {
+    return await listController.viewItems(request);
+  } else if (url.pathname.match("lists/[0-9]+/deactivate") && request.method === "POST") {
+    return await listController.deactivateList(request);
+  } else if (url.pathname.match("lists/[0-9]+/items/[0-9]+/collect") && request.method === "POST") {
+    return await itemController.collectItem(request);
+  } else if (url.pathname.match("lists/[0-9]+/items") && request.method === "POST") {
+    return await itemController.addItem(request);
+  } else {
+    return new Response("Not found", { status: 404 });
+  }
+};
+
+serve(handleRequest, { port: 7777 });
